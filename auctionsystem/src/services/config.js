0e2610401acdff115e09f874ed6d7faccb5cbc5c
lataonhehe <21020395@vnu.edu.vn>
2024-05-14 11:36:33 +0700

    Fix: change config firebase

--- auctionsystem/src/services/config.js
+++ auctionsystem/src/services/config.js
@@ -1,12 +1,22 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDGbaNjmoqflVWFcm3iSn1Hxp8X8TGAVQQ",
+//   authDomain: "catbid-cc5ae.firebaseapp.com",
+//   databaseURL: "https://catbid-cc5ae-default-rtdb.firebaseio.com",
+//   projectId: "catbid-cc5ae",
+//   storageBucket: "catbid-cc5ae.appspot.com",
+//   messagingSenderId: "1003892773947",
+//   appId: "1:1003892773947:web:3ea80a4e301cff03aff81e",
+//   measurementId: "G-WMFH4MMH5H",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDGbaNjmoqflVWFcm3iSn1Hxp8X8TGAVQQ",
-  authDomain: "catbid-cc5ae.firebaseapp.com",
-  databaseURL: "https://catbid-cc5ae-default-rtdb.firebaseio.com",
-  projectId: "catbid-cc5ae",
-  storageBucket: "catbid-cc5ae.appspot.com",
-  messagingSenderId: "1003892773947",
-  appId: "1:1003892773947:web:3ea80a4e301cff03aff81e",
-  measurementId: "G-WMFH4MMH5H",
+  apiKey: "AIzaSyCnNB59YjELs6qAs24Ob-xJnT1Ch-tkcVo",
+  authDomain: "dogbid-8c827.firebaseapp.com",
+  projectId: "dogbid-8c827",
+  storageBucket: "dogbid-8c827.appspot.com",
+  messagingSenderId: "28269640636",
+  appId: "1:28269640636:web:07e8b6168a11dbdcf809b9",
+  measurementId: "G-XD0EXTN5FJ"
 };
 
 export default firebaseConfig;
